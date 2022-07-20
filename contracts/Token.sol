d9cd722f6089cf2ec37beeb9bc40bab9a19c025e
ScottW3 <77562115+sw-3@users.noreply.github.com>
2022-07-20 16:59:11 -0500

    6. Token approvals

--- contracts/Token.sol
+++ contracts/Token.sol
@@ -12,10 +12,17 @@ contract Token {
 
 	// track balances with a key-value mapping; address = key, values are uint256
 	mapping(address => uint256) public balanceOf;
+	mapping(address => mapping(address => uint256)) public allowance;
 	
 	event Transfer(
 		address indexed from, 
 		address indexed to, 
+		uint256 value
+	);
+
+	event Approval(
+		address indexed owner,
+		address indexed spender,
 		uint256 value
 	);
 
@@ -41,7 +48,17 @@ contract Token {
 		balanceOf[_to] = balanceOf[_to] + _value;
 		// emit event
 		emit Transfer(msg.sender, _to, _value);
+		return true;
+	}
 
+	function approve(address _spender, uint256 _value) 
+		public 
+		returns(bool success)
+	{
+		require(_spender != address(0));
+
+		allowance[msg.sender][_spender] = _value;
+		emit Approval(msg.sender, _spender, _value);
 		return true;
 	}
 }
--- test/Token.js
+++ test/Token.js
@@ -8,7 +8,7 @@ const tokens = (n) => {
 }
 
 describe('Token', () => {
-	let token, accounts, deployer, receiver
+	let token, accounts, deployer, receiver, exchange
 
 	beforeEach(async () => {
 		// Fetch contract, deploy, and get Token from Blockchain
@@ -19,6 +19,7 @@ describe('Token', () => {
 		accounts = await ethers.getSigners()
 		deployer = accounts[0]
 		receiver = accounts[1]
+		exchange = accounts[2]
 	})
 
 	describe('Deployment', () => {
@@ -96,4 +97,36 @@ describe('Token', () => {
 
 	})
 	
+	describe('Approving Tokens', () => {
+		let amount, transaction, result
+
+		beforeEach(async () => {
+			amount = tokens(100)
+			transaction = await token.connect(deployer).approve(exchange.address, amount)
+			result = await transaction.wait()
+		})
+
+		describe('Success', () => {
+			it('allocates an allowance for delegated token spending', async () => {
+				expect(await token.allowance(deployer.address, exchange.address)).to.equal(amount)
+			})
+
+			it('emits an Approval event', async () => {
+				//console.log(result)   // to see entire logged tx from above
+				const event = result.events[0]
+				//console.log(event)    // to see the first event emitted
+				expect(event.event).to.equal('Approval')
+				const args = event.args
+				expect(args.owner).to.equal(deployer.address)
+				expect(args.spender).to.equal(exchange.address)
+				expect(args.value).to.equal(amount)
+			})			
+		})
+
+		describe('Failure', () => {
+			it('rejects invalid spenders', async () => {
+				await expect(token.connect(deployer).approve('0x0000000000000000000000000000000000000000', amount)).to.be.reverted
+			})
+		})
+	})
 })
