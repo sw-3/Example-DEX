2b170b3e6d1337c3eaad58b7f1adfebeb9db8f12
ScottW3 <77562115+sw-3@users.noreply.github.com>
2022-07-29 13:41:11 -0500

    16. Redux pt 1

--- src/components/App.js
+++ src/components/App.js
@@ -1,27 +1,27 @@
 import { useEffect } from 'react';
-import { ethers } from 'ethers';
+import { useDispatch } from 'react-redux'
 import config from '../config.json';
-import TOKEN_ABI from '../abis/Token.json'
-import '../App.css';
 
+import { 
+  loadProvider,
+  loadNetwork,
+  loadAccount,
+  loadToken
+} from '../store/interactions';
 
 function App() {
+  const dispatch = useDispatch()
 
   const loadBlockchainData = async () => {
     // standard method to talk to MetaMask wallet
-    const accounts = await window.ethereum.request({ method: 'eth_requestAccounts' })
-    console.log(accounts[0])
+    await loadAccount(dispatch)
 
     // connect ethers to blockchain
-    const provider = new ethers.providers.Web3Provider(window.ethereum)
-    const { chainId } = await provider.getNetwork()
-    console.log(chainId)
+    const provider = loadProvider(dispatch)
+    const chainId = await loadNetwork(provider, dispatch)
 
     // Token smart contract
-    const token = new ethers.Contract(config[chainId].DApp.address, TOKEN_ABI, provider)
-    console.log(token.address)
-    const symbol = await token.symbol()
-    console.log(symbol)
+    await loadToken(provider, config[chainId].DApp.address, dispatch)
   }
 
   useEffect(() => {
