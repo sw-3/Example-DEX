5cd69d0b4601d1f67bda64ad588559722a571de7
ScottW3 <77562115+sw-3@users.noreply.github.com>
2022-08-30 17:23:43 -0500

    28. My transactions

--- src/store/selectors.js
+++ src/store/selectors.js
@@ -194,6 +194,65 @@ const tokenPriceClass = (tokenPrice, orderId, previousOrder) => {
 }
 
 // -------------------------------------------------------------------------------------------
+// MY FILLED ORDERS
+
+export const myFilledOrdersSelector = createSelector(
+	account,
+	tokens,
+	filledOrders,
+	(account, tokens, orders) => {
+		// make sure we have both tokens loaded
+		if (!tokens[0] || !tokens[1]) { return }
+
+		// filter out my orders
+		orders = orders.filter((o) => o.user === account || o.creator === account)
+
+		// filter orders by selected tokens
+		orders = orders.filter((o) => o.tokenGet === tokens[0].address || o.tokenGet === tokens[1].address)
+		orders = orders.filter((o) => o.tokenGive === tokens[0].address || o.tokenGive === tokens[1].address)
+
+		// sort by date ascending
+		orders = orders.sort((a, b) => b.timestamp - a.timestamp)
+
+		// Decorate orders - add display attributes
+		orders = decorateMyFilledOrders(orders, account, tokens)
+
+		return(orders)
+	}
+)
+
+const decorateMyFilledOrders = (orders, account, tokens) => {
+	return(
+		orders.map((order) => {
+			order = decorateOrder(order, tokens)
+			order = decorateMyFilledOrder(order, account, tokens)
+			return(order)
+		})
+	)
+}
+
+const decorateMyFilledOrder = (order, account, tokens) => {
+	
+	// is this my order?
+	const myOrder = order.creator === account
+
+	// determine whether tokens were bought or sold by me
+	let orderType
+	if(myOrder) {
+		orderType = order.tokenGive === tokens[1].address ? 'buy' : 'sell'
+	} else {
+		orderType = order.tokenGive === tokens[1].address ? 'sell' : 'buy'
+	}
+
+	return({
+		...order,
+		orderType,
+		orderClass: (orderType === 'buy' ? GREEN : RED),
+		orderSign: (orderType === 'buy' ? '+' : '-')
+	})
+}
+
+// -------------------------------------------------------------------------------------------
 // ORDER BOOK
 
 // Note: createSelector Accepts one or more "input selectors" (either as separate arguments or 
